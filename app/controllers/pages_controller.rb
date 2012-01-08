9b0c6df1ada0a4a971d2ade366508910b8a0ab38
Jonathan Kapelus <jonathan@truckingsmart.co.za>
2012-01-08 10:07:05 +0200

    Removed default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
   	@title = "Home"
-  end
+	end
 
   def contact
   	@title = "Contact"
@@ -9,6 +9,9 @@ class PagesController < ApplicationController
 
   def about
 		@title = "About"
-  end
-  
+	end
+
+	def help
+		@title = "Help"
+	end
 end
